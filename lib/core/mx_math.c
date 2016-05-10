d62d32e1db4725b00df1433fa7e6371f52677a9f
Andrew Livesay <andrew.livesay@gmail.com>
2016-05-10 11:09:59 -0700

    o_o

--- lib/core/mx_math.c
+++ lib/core/mx_math.c
@@ -9,21 +9,23 @@ inline int64_t mx_math_min(const int64_t a, const int64_t b) {
 inline int64_t mx_math_max(const int64_t a, const int64_t b) {
     return (a < b) ? (b) : (a);
 }
-#include <stdio.h>
-inline uint64_t mx_math_np2(int64_t n) {
+inline uint64_t mx_math_np2(uint64_t n) {
     uint64_t i;
-    
-    n--;
+   
+    n = (n < 2) ? 2 : n;
 
+    n--;
     for (i = 1; i < (sizeof n << 2) + 1; i <<= 1) {
         n |= (n >> i);
     }
 
-    return ++n;
+    return ++n; 
 }
 
-inline uint64_t mx_math_pp2(int64_t n) {
+inline uint64_t mx_math_pp2(uint64_t n) {
     int64_t i;
+
+    n = (n < 3) ? 3 : n;
 
     for (i = 1; i < (sizeof n << 2) + 1; i <<= 1) {
         n |= (n >> i);
--- lib/core/mx_math.h
+++ lib/core/mx_math.h
@@ -10,8 +10,11 @@ int64_t mx_math_min(const int64_t a, const int64_t b);
 #define MX_MIN(a,b)  mx_math_min(a,b)
 int64_t mx_math_max(const int64_t a, const int64_t b);
 #define MX_MAX(a,b)  mx_math_max(a,b)
-uint64_t mx_math_np2(int64_t n);
-uint64_t mx_math_pp2(int64_t n);
-bool mx_math_isp2(const uint64_t);
+uint64_t mx_math_np2(uint64_t n);
+#define MX_NP2(n) mx_math_np2(n)
+uint64_t mx_math_pp2(uint64_t n);
+#define MX_PP2(n) mx_math_pp2(n)
+bool mx_math_isp2(const uint64_t n);
+#define MX_ISP2(n) mx_math_isp2(n)
 
 #endif // H_MXMATH
